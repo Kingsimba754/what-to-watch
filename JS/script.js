1c7a6e9756861f0e993878d6b26c81ef6e555301
Mohamed D. Ahmed <Mohamed@Mohameds-MBP-3.fios-router.home>
2022-12-01 20:10:24 -0500

    updates to HTML,CSS and JS

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,10 @@
-// IPO-Input -> process -> Output
+//IPO-Input -> process -> Output
 
 //Constants and variables  
- const API_URL = {
+const API_URL = {
 	"async": true,
 	"crossDomain": true,
-	"url": "https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=bobsburgers&country=uk",
+	"url": "https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=theoffice&country=uk",
 	"method": "GET",
 	"headers": {
 		"X-RapidAPI-Key": "bc08df07a1msh300c9fd8b57a6a6p1d7054jsn0439cf76e0d2",
@@ -12,11 +12,12 @@
 	}
 };
 
-$.ajax(API_URL).done(function (response) {
-	console.log(response);
-});
-//Cache DOM elements refrences
+// $.ajax(API_URL).then(function (data) {
+// 	console.log(data);
+// });
+
+
 
 //Register Event Listener
 
-//Functions
+Functions
