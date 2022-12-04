19b2975cc0f7f2194e4853546a65d7d8b61fcc3b
Mohamed D. Ahmed <Mohamed@Mohameds-MBP-3.fios-router.home>
2022-12-04 18:22:40 -0500

    linking data from API to HTML

--- JS/script.js
+++ JS/script.js
@@ -1,30 +1,40 @@
 //IPO-Input -> process -> Output
 
-//Constants and variables  
-const $title = ("#title")
-const $year = ("#year")
-const $rating = ("#Rating")
-const $awards = ("#awards")
-const $Description = ("#plot")
+//Constants and variables
+const $name = $("#name");
+const $StreamingService = $("#streaming-service");
+const $imdb = $("#imdb");
 
 const API_URL = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=theoffice&country=uk",
-	"method": "GET",
-	"headers": {
-		"X-RapidAPI-Key": "bc08df07a1msh300c9fd8b57a6a6p1d7054jsn0439cf76e0d2",
-		"X-RapidAPI-Host": "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com"
-	}
+  async: true,
+  crossDomain: true,
+  url: "https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=bobs-burgers&country=uk",
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "bc08df07a1msh300c9fd8b57a6a6p1d7054jsn0439cf76e0d2",
+    "X-RapidAPI-Host":
+      "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com",
+  },
 };
-function handleGetData(){
-$.ajax(API_URL).then(function (data) {
-	console.log(data);
-})
-};
+/////////
+//Functions
+/////////
+function handleGetData(event) {
+  // event.preventdefault();
+  $.ajax(API_URL).then(
+    function (data) {
+      console.log(data);
+      $name.text(data.results[0].name);
+      $StreamingService.text(data.results[0].locations[0].display_name);
+      $imdb.attr("href", data.results[0].external_ids.imdb.url);
+    },
+    (error) => {
+      console.log("bad request", error);
+    }
+  );
+}
 
-
-
+handleGetData();
 //Register Event Listener
 
 //Functions
